a3ffbdfab8e1c31bd01ec968ce1d07e1f36fe11f
curtisme <cmusgrave.evans@gmail.com>
2019-07-19 14:27:38 +1000

    Create Quine.ps1

---
+++ Quine.ps1
@@ -0,0 +1,1 @@
+function Q {param($string);$string-f[char]123,[char]36,[char]125,([char]34+$string+[char]34)};Q("function Q {0}param({1}string);{1}string-f[char]123,[char]36,[char]125,([char]34+{1}string+[char]34){2};Q({3})")
