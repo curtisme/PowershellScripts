f71db7841d8544c12fcc05c9da59fde84300dfdf
curtisme <cmusgrave.evans@gmail.com>
2022-01-28 10:55:11 +1100

    added logging

--- UrlDecodeDirectory.ps1
+++ UrlDecodeDirectory.ps1
@@ -5,6 +5,12 @@ foreach ($thing in ls)
     $newName = [System.Web.HttpUtility]::UrlDecode($thing.Name)
     if ($thing.name -ne $newName)
     {
+        write-host "renaming ""$($thing.name)"" to ""$newName..."""
         mv $thing.name $newName
+    }
+
+    else
+    {
+        write-host "skipping ""$newName""..."
     }
 }
