d7088a907f38ea2638c5c9eafae49f671a44c725
Curtis Musgrave-Evans <cmusgrave.evans@gmail.com>
2022-07-06 11:10:17 +1000

    updated to use hostname

--- TestTcpConnection.ps1
+++ TestTcpConnection.ps1
@@ -1,24 +1,18 @@
-function TestTcpConnection {
-    param([string]$ipaddressString, [int]$port)
+function TestTcpConnection
+{
+    param([string]$hostName, [int]$port)
     $streamType = [system.net.sockets.sockettype]::stream
     $tcpType = [system.net.sockets.protocoltype]::tcp
-    $octets = $ipaddressString.split('.')
-    $ipAddressInt64 = [int]$octets[0] * [math]::pow(256,0) +
-                      [int]$octets[1] * [math]::pow(256,1) +
-                      [int]$octets[2] * [math]::pow(256,2) +
-                      [int]$octets[3] * [math]::pow(256,3)
-    $ipaddress = new-object -typename system.net.ipaddress -argumentlist $ipaddressInt64
-    $endpoint = new-object -typename system.net.ipendpoint -argumentlist $ipaddress, $port
-    $socket = new-object -typename system.net.sockets.socket -argumentlist $ipaddress.addressfamily, $streamType, $tcpType
-    try {
-        $socket.connect($endpoint)
+    $addressFamily = [system.net.sockets.addressfamily]::internetwork
+    $socket = new-object -typename system.net.sockets.socket -argumentlist $addressFamily, $streamType, $tcpType
+    try
+    {
+        $socket.connect($hostname, $port)
         $socket.close()
         write-host "TCP connection succesfully established"
     }
-    catch {
+    catch
+    {
         write-host $_
     }
 }
-
-
-TestTcpConnection $args[0] $args[1]
